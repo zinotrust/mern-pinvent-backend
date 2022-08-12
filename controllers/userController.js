5eda7e0ab9d6d95be84d467c9b1674d1007905ab
ZinoTrust <zinotrust@gmail.com>
2022-08-12 14:05:13 +0100

    Register and Login function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,79 @@
 const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-const registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
+// Generate token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
 };
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, photo } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please fill in all fields");
+  }
+
+  // Check if user exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+    photo,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      photo: user.photo,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+// Login User
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  // Check DB if user exists
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      photo: user.photo,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid email or password");
+  }
+});
 
 module.exports = {
   registerUser,
+  loginUser,
 };
--- models/userModel.js
+++ models/userModel.js
@@ -17,6 +17,12 @@ const userSchema = mongoose.Schema(
       required: [true, "Please add a password"],
       minLength: [6, "Password must be up to 6 characters"],
     },
+    photo: {
+      type: String,
+      required: [true, "Please add a photo"],
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
   },
   {
     timestamps: true,
